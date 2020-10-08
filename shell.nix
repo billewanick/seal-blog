e3db7185afe36f0c2f1e26dc0334544041e6854d
Bill Ewanick <bill@ewanick.com>
2020-10-07 21:35:33 -0400

    Finally downgrade Hakyll so it'll compile!!

--- shell.nix
+++ shell.nix
@@ -4,13 +4,23 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
+  pkgs = import (builtins.fetchGit {
+       # Descriptive name to make the store path easier to identify                
+       name = "hakyll-4.13.3.0";                                                 
+       url = "https://github.com/nixos/nixpkgs-channels/";                       
+       ref = "refs/heads/nixpkgs-unstable";                     
+       rev = "fa54dd346fe5e73d877f2068addf6372608c820b";                                           
+  }) {};                                                                           
+
+  myPkg = pkgs.haskellPackages.hakyll;
+
   ghc = pkgs.haskellPackages.ghcWithHoogle (self: with self; [
-          hspec
-          split
-          hakyll
-          random
-          neat-interpolation
-        ]);
+        hspec
+        split
+        myPkg # hakyll
+        random
+        neat-interpolation
+      ]);
 in
 pkgs.mkShell {
   name = "haskell-shell";
