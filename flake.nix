7aa1e8e19dac2c8f8d48b7b44a0a855d434deb95
Bill Ewanick <bill@ewanick.com>
2023-08-02 20:22:52 -0400

    Add 'nix run .#generateSealPosts' input and command

--- flake.nix
+++ flake.nix
@@ -3,10 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+
     utils.url = "github:numtide/flake-utils";
+
+    spg.url = "git+https://git.ewanick.com/bill/sealPostGenerator.git";
+    spg.inputs.nixpkgs.follows = "nixpkgs";
+    spg.inputs.utils.follows = "utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs, utils, ... }@inputs:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -20,19 +25,23 @@
         ]);
       in
       {
+        apps.generateSealPosts = {
+          type = "app";
+          program = "${inputs.spg.packages.${system}.default}/bin/generateSealPosts";
+        };
+
         devShells.default = pkgs.mkShell {
           name = "hakyll-shell";
 
-          buildInputs = with pkgs.haskellPackages;
-            [
-              ghc'
-              hlint
-              haskell-language-server
+          buildInputs = with pkgs.haskellPackages; [
+            ghc'
+            hlint
+            haskell-language-server
 
-              (pkgs.writeShellScriptBin "build-site" ''
-                ${ghc'}/bin/ghc --make website/site -outputdir dist -static -O2
-              '')
-            ];
+            (pkgs.writeShellScriptBin "build-site" ''
+              ${ghc'}/bin/ghc --make website/site -outputdir dist -static -O2
+            '')
+          ];
         };
       });
 }
